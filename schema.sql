340b5b02b1c0eb8c4c3d352f145a8881637a6c02
Linda Keza <lindakeza19@gmail.com>
2022-06-02 12:30:45 +0200

    Create table item and add it to the project

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+      id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attemps INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL 
 );
