7ff49c2a2cd92c1d1aa3f521afaaed33cb4c3bb4
Linda Keza <lindakeza19@gmail.com>
2022-06-02 14:43:48 +0200

    Create table named owners, and species

--- schema.sql
+++ schema.sql
@@ -11,3 +11,29 @@ CREATE TABLE animals (
 
 ALTER TABLE animals ADD species VARCHAR(100); 
 
+--Creating a table named owners
+CREATE TABLE owners(
+   id BIGSERIAL PRIMARY KEY,
+   full_name VARCHAR(255),
+   age INT
+);
+
+--Create a table named species
+CREATE TABLE species(
+   id BIGSERIAL,
+   name VARCHAR(255),
+   PRIMARY KEY(id)
+   );
+
+   --Modify animals table:
+
+--Remove column species
+ALTER TABLE animals DROP COLUMN species;
+
+--Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+
+--Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id); 
