10a3016a917b1e5c6466987ffeb8349c4bb5f03f
Linda Keza <lindakeza19@gmail.com>
2022-06-02 20:14:56 +0200

    Insert new data into vet_clinic database

--- data.sql
+++ data.sql
@@ -32,3 +32,26 @@ UPDATE animals SET owner_id=3 WHERE name IN ('Devimon','Plantmon');
 UPDATE animals SET owner_id=4 WHERE name IN ('Charmander','Squirtle','Blossom');
 
 UPDATE animals SET owner_id=5 WHERE name IN ('Angemon','Boarmon'); 
+
+-- Insert the following data for vets:
+       -- Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+       -- Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+       -- Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+       -- Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+INSERT INTO vets(name,age,date_of_graduation) VALUES('William Tatche', 45,'2000-04-23'),
+(' Maisy Smith', 26, '2019-01-17'),('Stephanie Mendez',64,'1981-05-04'),
+('Jack Harkness', 38,'2008-06-08');
+
+-- Insert the following data for specialties:
+    -- Vet William Tatcher is specialized in Pokemon.
+    -- Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+    -- Vet Jack Harkness is specialized in Digimon.
+INSERT INTO specializations (species_id,vets_id) VALUES (1,1),(2,3),(1,3),(2,4);
+
+
+-- Insert the following data for visits:
+INSERT INTO visits (animals_id,vets_id,date_of_visit) VALUES (1, 1, '2020-05-24'),(1, 3, '2020-07-22'),(2, 4, '2021-02-02'),(3, 2, '2020-01-05');
+INSERT INTO visits (animals_id,vets_id,date_of_visit) VALUES (3, 2, '2020-03-08'),(3, 2, '2020-05-14'),(4, 2, '2021-05-04'),(5, 4, '2021-02-24');
+INSERT INTO visits (animals_id,vets_id,date_of_visit) VALUES (6, 2, '2019-12-21'),(6, 1, '2020-08-10'),(6, 2, '2021-04-07'),(7, 3, '2019-09-29');
+INSERT INTO visits (animals_id,vets_id,date_of_visit) VALUES (8, 4, '2020-10-03'),(8, 4, '2020-11-04'),(9, 2, '2019-01-24'),(9, 2, '2019-05-15');
+INSERT INTO visits (animals_id,vets_id,date_of_visit) VALUES (9, 2, '2020-02-27'),(9, 2, '2020-08-03'),(10, 3, '2020-05-24'),(10, 1, '2021-01-11'); 
